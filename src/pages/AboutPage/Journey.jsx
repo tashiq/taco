9dec1ba4466b1a66f154738570d73673ff7d6c28
tashiq <fahim.csecu@gmail.com>
2025-02-27 12:22:56 +0600

    responsive except lines

--- src/pages/AboutPage/Journey.jsx
+++ src/pages/AboutPage/Journey.jsx
@@ -17,8 +17,8 @@ const Journey = () => {
   const [twoFourBtn, setTwoFourBtn] = useState(false);
 
   return (
-    <div className="p-12 flex gap-12 justify-between items-center bg-black text-white mb-1 z-10 relative">
-      <div className="bg-white text-black flex flex-col gap-3 items-start justify-start w-3/12 p-4 rounded">
+    <div className="p-4 lg:p-12 flex flex-col lg:flex-row gap-2 lg:gap-12 justify-between items-center bg-black text-white mb-1 z-10 relative">
+      <div className="bg-white text-black flex flex-col gap-3 items-start justify-start w-full lg:w-3/12 p-2 lg:p-4 rounded">
         <h1 className="text-3xl font-bold">Our Journey</h1>
         <p>
           Since 2004, MMG has grown into a diversified conglomerate, driven by
@@ -27,28 +27,36 @@ const Journey = () => {
           demands.
         </p>
       </div>
-      <div className="h-full flex items-end w-9/12 gap-4 overflow-hidden">
+      <div className="h-full flex flex-row items-end w-full lg:w-9/12 gap-4 ">
         {clicked && (
-          <button onClick={() => setClicked((prev) => !prev)}>
+          <button
+            className="hidden lg:block translate-y-4"
+            onClick={() => setClicked((prev) => !prev)}
+          >
             <img src="/arrow left.png" alt="" />
           </button>
         )}
-        <div className="flex w-11/12">
-          <div className={`flex w-full ${clicked && "hidden"}`}>
-            <div className="min-w-64">
-              <h1 className="h-3/12">2004</h1>
-              <div className="relative my-8 ml-4 h-0 border-b-2 border-white">
-                <img
-                  src="/down.png"
-                  className=" rounded-full bg-black  border-2 border-white -translate-y-1/2 p-1"
-                  style={{ width: "24px", height: "24px" }}
-                  onClick={() => {
-                    setFourBtn(false);
-                    setFour(
-                      "Our foundation was laid with the launch of MMG’s Industrial Solutions, partnering with global"
-                    );
-                  }}
-                />
+        <div className="flex flex-col lg:flex-row w-full lg:w-11/12">
+          <div
+            className={`flex flex-col lg:flex-row w-full ${
+              clicked && "hidden"
+            }`}
+          >
+            <div className="min-w-64  border-l-2 lg:border-l-0 border-white pl-4 lg:pl-0 pb-4">
+              <h1 className="h-3/12 font-medium">2004</h1>
+              <div className="relative my-2 lg:my-8 h-0 border-b-0 lg:border-b-2 border-white">
+                <div className="border-2 border-white w-[30px] h-[30px] rounded-full bg-black -translate-y-1/2 p-[3px] pt-[6px] absolute -left-8 lg:left-2 -rotate-90 lg:rotate-0">
+                  <img
+                    src="/down.png"
+                    className="w-[30px] h-[15px]"
+                    onClick={() => {
+                      setFourBtn(false);
+                      setFour(
+                        "Our foundation was laid with the launch of MMG’s Industrial Solutions, partnering with global"
+                      );
+                    }}
+                  />
+                </div>
               </div>
               <p className="pr-4 pl-2">
                 <p className="font-bold">Industrial Solutions Division</p>
@@ -68,18 +76,21 @@ const Journey = () => {
                 </button>
               )}
             </div>
-            <div className="min-w-64">
-              <h1 className="h-3/12">2016</h1>
-              <div className="relative my-8 h-0 border-b-2 border-white">
-                <img
-                  src="/down.png"
-                  className="ml-3 rounded-full bg-black  border-2 border-white -translate-y-1/2 p-1"
-                  style={{ width: "24px", height: "24px" }}
-                  onClick={() => {
-                    setSixteenBtn(false);
-                    setSixteen("We entered the fisheries industry, becoming a");
-                  }}
-                />
+            <div className="min-w-64  border-l-2 lg:border-l-0 border-white pl-4 lg:pl-0 pb-4">
+              <h1 className="h-3/12 font-medium">2016</h1>
+              <div className="relative my-2 lg:my-8 h-0 border-b-0 lg:border-b-2 border-white">
+                <div className="border-2 border-white w-[30px] h-[30px] rounded-full bg-black -translate-y-1/2 p-[3px] pt-[6px] absolute -left-8 lg:left-2 -rotate-90 lg:rotate-0">
+                  <img
+                    src="/down.png"
+                    className="w-[30px] h-[15px]"
+                    onClick={() => {
+                      setSixteenBtn(false);
+                      setSixteen(
+                        "We entered the fisheries industry, becoming a"
+                      );
+                    }}
+                  />
+                </div>
               </div>
               <p className="pr-4 pl-2">
                 <p className="font-bold">Fisheries Business</p>
@@ -99,20 +110,21 @@ const Journey = () => {
                 </button>
               )}
             </div>
-            <div className="min-w-64">
-              <h1 className="h-3/12">2024</h1>
-              <div className="relative my-8 h-0 border-b-2 border-white">
-                <img
-                  src="/down.png"
-                  className="ml-3 rounded-full bg-black  border-2 border-white -translate-y-1/2 p-1"
-                  style={{ width: "24px", height: "24px" }}
-                  onClick={() => {
-                    setTwoFourBtn(false);
-                    setTwoFour(
-                      "With MM Seaways, we extended our expertise to logistics, offering freight forwarding"
-                    );
-                  }}
-                />
+            <div className="min-w-64  border-l-2 lg:border-l-0 border-white pl-4 lg:pl-0 pb-4">
+              <h1 className="h-3/12 font-medium">2024</h1>
+              <div className="relative my-2 lg:my-8 h-0 border-b-0 lg:border-b-2 border-white">
+                <div className="border-2 border-white w-[30px] h-[30px] rounded-full bg-black -translate-y-1/2 p-[3px] pt-[6px] absolute -left-8 lg:left-2 -rotate-90 lg:rotate-0">
+                  <img
+                    src="/down.png"
+                    className="w-[30px] h-[15px]"
+                    onClick={() => {
+                      setTwoFourBtn(false);
+                      setTwoFour(
+                        "With MM Seaways, we extended our expertise to logistics, offering freight forwarding"
+                      );
+                    }}
+                  />
+                </div>
               </div>
               <p className="pr-4 pl-2">
                 <p className="font-bold">MM Seaways LTD.</p>
@@ -132,6 +144,14 @@ const Journey = () => {
                 </button>
               )}
             </div>
+            <div className="block lg:hidden w-72 mt-8">
+              <h1 className="mb-2 text-2xl font-bold">Beyond the Core</h1>
+              <p>
+                We also explore other sectors, including Power & Energy,
+                Automobile Dealerships, Software Solutions, Real Estate,
+                Investments.
+              </p>
+            </div>
           </div>
           <div className={`w-72 ${!clicked && "hidden"}`}>
             <h1 className="mb-6 text-2xl font-bold">Beyond the Core</h1>
@@ -143,7 +163,10 @@ const Journey = () => {
           </div>
         </div>
         {!clicked && (
-          <button onClick={() => setClicked((prev) => !prev)}>
+          <button
+            className="hidden lg:block"
+            onClick={() => setClicked((prev) => !prev)}
+          >
             <img src="/arrow right.png" alt="" />
           </button>
         )}
